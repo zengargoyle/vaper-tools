2bb36ac74a21764a09c98d0b34a0795e6b07ec75
zengargoyle <zengargoyle@gmail.com>
2013-08-29 21:08:50 -0700

    move amt() into Simple

--- lib/App/JuiceCalc/Simple.pm
+++ lib/App/JuiceCalc/Simple.pm
@@ -2,10 +2,7 @@ use strict;
 use warnings;
 
 package App::JuiceCalc::Simple;
-
 use Exporter::Lite;
-
-use App::JuiceCalc;
 
 our @EXPORT = qw(
   name
@@ -20,19 +17,26 @@ our @EXPORT = qw(
   batch
   batch_generate
   batch_reset
+  amt
+  amt_reset
+  amt_generate
 );
+
+use List::AllUtils qw( sum first );
+use Scalar::Util qw( looks_like_number );
+
+use App::JuiceCalc;
 
 {
 
   my ($fb, $mix, $batch);
+  my @amt;
 
   sub batch_reset {
     $fb = FlavorBundle->new;
     $mix = Mix->new( flavor => $fb );
     $batch = Batch->new( mix => $mix );
   }
-
-  batch_reset();
 
   sub name { $fb->name( shift ) }
 
@@ -50,13 +54,9 @@ our @EXPORT = qw(
   }
 
   sub fill { $mix->ratio([ @_ ]) }
-  use Scalar::Util qw( looks_like_number );
-  use List::AllUtils qw( first );
-  #use Data::Dump;
 
   sub size {
     $batch->size( first { looks_like_number $_ } @_, 10 );
-    #dd [ $batch ];
   }
 
   sub mix { $mix }
@@ -70,5 +70,44 @@ our @EXPORT = qw(
   sub emitter { $batch->emitter( @_ ) }
 
   sub list_emitters { $batch->list_emitters(1) }
+
+  sub amt_reset { @amt = () }
+
+  sub amt {
+    push @amt, [ @_ ] if @_;
+  }
+
+  sub amt_generate {
+
+    my $s = sum map $_->[1], @amt;
+    $_->[1] /= $s for @amt;
+
+    my %fr = ( pg => 1, vg => 0 );
+
+    for my $f ( @amt ) {
+      if ($f->[0] =~ /^nic(:?otine)?$/i) {
+        my (undef, $pc, %rest) = @{ $f };
+        my $nic = delete $rest{mg} // 0;
+        my $mg = $nic * $pc;
+        nic($mg, $nic, %rest);
+        next;
+      }
+      if ($f->[0] =~ /^(pg|vg)$/i) {
+        $fr{ uc($1) } = $f->[1];
+        next;
+      }
+      my (undef, $pc, %rest) = @{ $f };
+      %rest = ( pg => 1, %rest );
+      $fr{$_} += $pc * $rest{$_} for keys %rest;
+      flav(@{ $f });
+    }
+    fill(vg => $fr{vg} + $fr{VG}, pg => 1);
+  }
+
+  # redundant
+  batch_reset();
+  amt_reset();
+
 }
+
 1;
